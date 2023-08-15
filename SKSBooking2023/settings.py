881790ca9e0af16723e9b0ad3c5f6560fbdaa51a
Markus Glavind <glavindmarkus@gmail.com>
2023-08-16 00:07:03 +0200

    update basedir

--- SKSBooking2023/settings.py
+++ SKSBooking2023/settings.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()  # loads the configs from .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
